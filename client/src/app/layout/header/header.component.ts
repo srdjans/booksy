65a7f2cb80fec9a1c133ea579e139fd39633c13a
Srdjan S <srdjan206@hotmail.com>
2026-01-06 17:58:35 -0800

    Added menu button for mobile size. More responsiveness improvements

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { MatBadge } from "@angular/material/badge";
-import { MatButton } from "@angular/material/button";
+import { MatButton, MatIconButton } from "@angular/material/button";
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
     MatIcon,
     MatButton,
-    MatBadge
+    MatBadge,
+    MatIconButton,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  mobileMenuOpen = false;
 
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
 }
